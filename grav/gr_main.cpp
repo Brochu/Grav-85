d13bfa4d530eb6b8348f886e749230ffc78946c4
Alex Brochu <alexbrochu1@gmail.com>
2026-02-02 10:04:25 -0800

    Some progress with simple game mecacnis and level handling, next hanlding current run for specific level

--- grav/gr_main.cpp
+++ grav/gr_main.cpp
@@ -2,6 +2,7 @@
 
 #include "qg_bus.hpp"
 #include "qg_config.hpp"
+#include "qg_math.hpp"
 #include "qg_memory.hpp"
 #include "qg_parse.hpp"
 #include "qg_random.hpp"
@@ -11,46 +12,85 @@
 
 engine_api g_api {};
 
-enum class space : i8 {
-    EMPTY,
+enum class space : u8 {
     SOLID,
-    BLOCK,
-    STONE,
+    EMPTY,
+    CRATE,
+    GEM,
 };
 
-enum class color : i8 {
+enum class color : u8 {
     RED,
     GREEN,
     BLUE,
 };
 
-enum class direction : i8 {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT,
-};
-
-#define STONES_MAX_NUM 32
-#define MAP_MAX_SIZE 32*32
+#define GEMS_MAX_NUM 32
+#define MAP_MAX_SIZE 16*16
+#define LEVEL_FILE_SIZE 76
 
 struct level {
     i8 width;
     i8 height;
     direction start_gravity;
-    color stone_colors[STONES_MAX_NUM];
+
+    i8 num_colors;
+    color gem_colors[GEMS_MAX_NUM];
+
     space map[MAP_MAX_SIZE];
 };
 
-void level_file_init(level *lvl, const char *file_path) { }
+void level_file_init(level *lvl, const char *file_path) {
+    FILE *lvl_file;
+    i32 err = fopen_s(&lvl_file, file_path, "rb");
+    assert(err == 0);
+
+    u8 lvl_data[LEVEL_FILE_SIZE];
+    u64 read_len = fread_s(lvl_data, LEVEL_FILE_SIZE, LEVEL_FILE_SIZE, 1, lvl_file);
+    assert(read_len >= 1);
+
+    lvl->width = (i8)lvl_data[0];
+    lvl->height = (i8)lvl_data[1];
+    lvl->start_gravity = (direction)lvl_data[2];
+
+    lvl->num_colors = (i8)lvl_data[3];
+    u64 colors_data = *(u64*)&lvl_data[4];
+    for (int i = 0; i < lvl->num_colors; i++) {
+        lvl->gem_colors[i] = (color)((colors_data >> (2 * i)) & 0b11);
+    }
+
+    u64 spaces_data[8];
+    for (int i = 0; i < 8; i++) {
+        spaces_data[i] = *(u64*)&lvl_data[12 + i * 8];
+    }
+
+    int total_spaces = lvl->width * lvl->height;
+    for (int i = 0; i < total_spaces; i++) {
+        int chunk = i / 32;
+        int bit_pos = (i % 32) * 2;
+        lvl->map[i] = (space)((spaces_data[chunk] >> bit_pos) & 0b11);
+    }
+}
 void level_data_init(level *lvl, arena_ptr data) { }
+
+space level_space_at(level *lvl, ivec2 pos) {
+    u32 idx = (pos.y * lvl->width) + pos.x;
+    return lvl->map[idx];
+}
 
 struct run {
     direction current_gravity;
-    //TODO: How do we represent the current map status - based off of map from level
+
+    ivec2 crates[GEMS_MAX_NUM];
+    ivec2 gems[GEMS_MAX_NUM];
 };
 
-void run_level_init(run *run, level *lvl) { }
+void run_level_init(run *run, level *lvl) {
+    run->current_gravity = lvl->start_gravity;
+    //TODO: create positions based off of the map definition
+}
+
+level g_lvl;
 
 void grav_init(engine_api api) {
     g_api = api;
@@ -99,16 +139,26 @@ void grav_init(engine_api api) {
     evt.level_name = "LEVEL0";
 
     g_api.bus_fire(g_api.bus, event_type::LEVEL_LOADED, &evt, sizeof(evt));
+
+    level_file_init(&g_lvl, "assets/demo_level.bin");
 }
 
 void grav_tick(f32 dt) {
 }
 
 void grav_draw(f32 dt) {
-    SDL_SetRenderDrawColor(g_api.context, 0, 127, 0, 255);
+    SDL_SetRenderDrawColor(g_api.context, 127, 127, 127, 255);
+    f32 cell_size = 32;
 
-    SDL_FRect r { 0, 0, 100, 100 };
-    SDL_RenderFillRect(g_api.context, &r);
+    for (i32 y = 0; y < g_lvl.height; y++) {
+        for (i32 x = 0; x < g_lvl.width; x++) {
+            ivec2 p { x, y };
+            if (level_space_at(&g_lvl, p) == space::SOLID) {
+                SDL_FRect r { x*cell_size, y*cell_size, cell_size-5, cell_size-5 };
+                SDL_RenderFillRect(g_api.context, &r);
+            }
+        }
+    }
 }
 
 void grav_exit() {
