9ad02f3cb1cdff6bb9a604eb6711381ac544f9f4
Alex Brochu <alexbrochu1@gmail.com>
2026-02-05 11:52:42 -0800

    Got first version of gravity change logic to move elements around the level.

--- grav/gr_main.cpp
+++ grav/gr_main.cpp
@@ -9,6 +9,7 @@
 #include "SDL3/SDL.h"
 
 engine_api g_api {};
+input_state *g_in {};
 
 enum class color : u8 {
     RED,
@@ -94,7 +95,7 @@ struct run {
     u64 start_timestamp;
     ivec2 crates[ELEMENTS_MAX_NUM];
     ivec2 gems[ELEMENTS_MAX_NUM];
-    vec2 crate_offsets[ELEMENTS_MAX_NUM];  // visual offset from logical pos (animates toward zero)
+    vec2 crate_offsets[ELEMENTS_MAX_NUM];
     vec2 gem_offsets[ELEMENTS_MAX_NUM];
     direction moves[RUN_MAX_MOVES];
     direction current_gravity;
@@ -133,11 +134,77 @@ void run_level_reset(run *run, level *lvl) {
     run->num_moves = 0;
 }
 
+bool run_element_at(run *run, ivec2 pos) {
+    for (i32 i = 0; i < run->num_crates; i++) {
+        if (run->crates[i] == pos) {
+            return true;
+        }
+    }
+
+    for (i32 i = 0; i < run->num_gems; i++) {
+        if (run->gems[i] == pos) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void run_gravity_change(run *run, level *lvl, direction new_gravity) {
+    //TODO: Do we need to store the level in the run itself?
+    if (new_gravity == run->current_gravity) {
+        return;
+    }
+
+    run->current_gravity = new_gravity;
+    ivec2 dir = direction_vectors[(u8)new_gravity];
+    ivec2 opp = direction_vectors[((u8)new_gravity + 2) % 4];
+    i32 num_moves = 0;
+
+    auto update_func = [&](ivec2 *positions, vec2 *offsets, i32 i) {
+        ivec2 start = positions[i];
+        ivec2 end = start;
+        ivec2 push = ivec2_zero;
+
+        while (!level_is_solid(lvl, end)) {
+            if (end != start && run_element_at(run, end)) {
+                push = push + opp;
+            }
+
+            end = end + dir;
+        }
+        end = end + opp;
+
+        if ((end + push) != start) {
+            num_moves++;
+            positions[i] = end + push;
+            offsets[i] = to_vec2(start - positions[i]);
+        }
+    };
+
+    for (i32 i = 0; i < run->num_crates; i++) {
+        update_func(run->crates, run->crate_offsets, i);
+    }
+    for (i32 i = 0; i < run->num_gems; i++) {
+        update_func(run->gems, run->gem_offsets, i);
+    }
+
+    //TODO: Double check lambda approach
+    if (num_moves > 0) {
+        run->animating = true;
+    }
+}
+
+void run_check_combos(run *run) {
+    //TODO: Flood fill to check for gems that should de-activate
+}
+
 level g_lvl;
 run g_run;
 
 void grav_init(engine_api api) {
     g_api = api;
+    g_in = api.input;
 
     config cfg;
     g_api.config_init(&cfg, "assets/game.cfg");
@@ -156,14 +223,41 @@ void grav_init(engine_api api) {
 }
 
 void grav_tick(f32 dt) {
-    if (g_api.input_pressed(g_api.input, input_action::RESET)) {
-        g_lvl.gem_colors[0] = color::RED;
-    } else {
-        g_lvl.gem_colors[0] = color::GREEN;
+    if (!g_run.animating  && g_api.input_pressed(g_in, input_action::GRAVITY_UP)) {
+        run_gravity_change(&g_run, &g_lvl, direction::UP);
+    }
+    else if (!g_run.animating  && g_api.input_pressed(g_in, input_action::GRAVITY_RIGHT)) {
+        run_gravity_change(&g_run, &g_lvl, direction::RIGHT);
+    }
+    else if (!g_run.animating  && g_api.input_pressed(g_in, input_action::GRAVITY_DOWN)) {
+        run_gravity_change(&g_run, &g_lvl, direction::DOWN);
+    }
+    else if (!g_run.animating  && g_api.input_pressed(g_in, input_action::GRAVITY_LEFT)) {
+        run_gravity_change(&g_run, &g_lvl, direction::LEFT);
+    }
+
+    if (g_run.animating) {
+        // Move elements towards offset == 0
     }
 }
 
 void grav_draw(f32 dt) {
+    // Render gravity indicator
+    f32 center_x = 600.0f, center_y = 30.0f;
+    f32 len = 15.0f;
+    ivec2 dir = direction_vectors[(i32)g_run.current_gravity];
+    f32 dx = (f32)dir.x * len;
+    f32 dy = (f32)dir.y * len;
+
+    SDL_SetRenderDrawColor(g_api.context, 255, 255, 0, 255);  // yellow
+    SDL_RenderLine(g_api.context, center_x - dx, center_y - dy, center_x + dx, center_y + dy);
+    // arrowhead
+    f32 ax = (f32)(-dir.y) * 6.0f;
+    f32 ay = (f32)(dir.x) * 6.0f;
+    SDL_RenderLine(g_api.context, center_x + dx, center_y + dy, center_x + dx*0.5f + ax, center_y + dy*0.5f + ay);
+    SDL_RenderLine(g_api.context, center_x + dx, center_y + dy, center_x + dx*0.5f - ax, center_y + dy*0.5f - ay);
+
+    // Render solid walls
     SDL_SetRenderDrawColor(g_api.context, EXPAND_COLOR(WALL_COLOR_INDEX));
     f32 cell_size = 32;
 
@@ -177,6 +271,7 @@ void grav_draw(f32 dt) {
         }
     }
 
+    // Render elements crates and gems
     SDL_SetRenderDrawColor(g_api.context, EXPAND_COLOR(CRATE_COLOR_INDEX));
     for (i32 i = 0; i < g_run.num_crates; i++) {
         auto [x, y] = g_run.crates[i];
